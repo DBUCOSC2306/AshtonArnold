404689f29fb9c782a04290950c0ecb96d8f14f82
AshtonArnold <ashtonlynn2013@gmail.com>
2017-03-09 16:05:36 -0600

    Revert "Revert "Chapter 5 Homework""
    This reverts commit d95adf4760c75d822f695eab96089bc54f024f0b.

--- Chapter5_HW/src/chapter5HWproject/Chapter5HW.java
+++ Chapter5_HW/src/chapter5HWproject/Chapter5HW.java
@@ -1,5 +1,7 @@
 package chapter5HWproject;
- 
+import jsjf.CircularArrayQueue;
+import jsjf.LinkedQueue;
+import jsjf.exceptions.*;
 /**
  *
  * @author jackjt
@@ -11,9 +13,68 @@ public class Chapter5HW {
      */
     public static void main(String[] args) {
         
-	// 1. Complete the CirucularArrayQueue and LinkedQueue classes
-        // 2. In this main, write a test for each method AND any exceptions
-        // 3. Push all of the changes back to the repository (git push)
+        CircularArrayQueue C1 = new CircularArrayQueue(5);
+        LinkedQueue L1 = new LinkedQueue();
+        
+     System.out.println("Is this queue empty? " + C1.isEmpty());
+     //Add elements
+     C1.enqueue(87);
+     C1.enqueue(88);
+     C1.enqueue(89);
+     C1.enqueue(90);
+     C1.enqueue(91);
+     
+     //test size
+     int C1Size = C1.size();
+     System.out.println("There is " + C1Size + " elements currently in the queue.");
+     System.out.println("This is the queue current standing: " + C1.toString());
+     System.out.println("Now lets delete some...");
+     
+     C1.dequeue();
+     C1.dequeue();
+     C1.dequeue();
+     System.out.println("Is this queue empty? " + C1.isEmpty());
+     System.out.println("This is the new front: " + C1.first()); 
+     //System.out.println("This is the array current standing: " + C1.toString());
+     System.out.println("Now lets delete the rest...");
+     C1.dequeue();
+     C1.dequeue();
+     System.out.println("Is this array empty? " + C1.isEmpty());
+     
+     //C1.dequeue(); //test dequeue() exception
+     System.out.println("We're going to test the first element exception " );
+     C1.first(); //test first() exception
+     
+     
+     System.out.println("\nNow lets test the LinkedQueue!");
+     System.out.println("Lets add some elements to this queue ");
+     L1.enqueue(8);
+     L1.enqueue(9);
+     L1.enqueue(10);
+     L1.enqueue(11);
+     L1.enqueue(12);
+     L1.enqueue(13);
+     
+     int L1Size = L1.size();
+     System.out.println("There is " + L1Size + " elements currently in the queue.");
+     System.out.println("This is the queue current standing: " + L1.toString());
+     System.out.println("Now lets delete some...");
+     
+     L1.dequeue();
+     L1.dequeue();
+     L1.dequeue();
+     System.out.println("There is " + L1Size + " elements currently in the queue.");
+     System.out.println("This is the queue current standing: " + L1.toString());
+     System.out.println("Now lets delete the rest...");
+     L1.dequeue();
+     L1.dequeue();
+     L1.dequeue();
+     System.out.println("Is the queue empty? " + L1.isEmpty());
+     //L1.dequeue(); //test dequeue() exception
+     System.out.println("We're going to test the first element exception " );
+     L1.first();
+     
+
         
     }
 }
